204293e27e31a9d888232b21de09c68468baf08a
Gustavo Lima <correia.gustavol@gmail.com>
2024-08-02 15:49:41 -0300

    Small streamlit changes

--- streamlit-ui/app.py
+++ streamlit-ui/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-st.title("TubeSage - Ask anything about YouTube videos")
+st.title(":mage: TubeSage - Ask anything about YouTube videos")
 
 
 # Initialize messages in session state
@@ -25,6 +25,7 @@ youtube_url = st.text_input("Enter YouTube URL")
 if st.button("Fetch Transcript"):
     if youtube_url:
         transcript = fetch_youtube_transcript(youtube_url)
+        st.info("Video Transcribed, you can start chatting with the assistant now!")
     else:
         st.warning("Please enter a YouTube URL")
 
@@ -34,17 +35,18 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # Handle user input and API response
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+if youtube_url:
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-    # Make a stateless API call to your backend API with the last user message
-    response = requests.post("http://tubesage:5000/v1/invoke", json={"input": prompt, "video_url": youtube_url})
-    if response.status_code == 200:
-        assistant_response = response.json().get("response", "")
-        st.session_state.messages.append({"role": "assistant", "content": assistant_response})
-        with st.chat_message("assistant"):
-            st.markdown(assistant_response)
-    else:
-        st.error("Failed to fetch response from the API")
+        # Make a stateless API call to your backend API with the last user message
+        response = requests.post("http://tubesage:5000/v1/invoke", json={"input": prompt, "video_url": youtube_url})
+        if response.status_code == 200:
+            assistant_response = response.json().get("response", "")
+            st.session_state.messages.append({"role": "assistant", "content": assistant_response})
+            with st.chat_message("assistant"):
+                st.markdown(assistant_response)
+        else:
+            st.error("Failed to fetch response from the API")
